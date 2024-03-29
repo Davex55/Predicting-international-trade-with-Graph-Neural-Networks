da80c1122419125e6bacf43fb9b566d662807a01
Alejandro Benavente <contacto@alexbenavente.com>
2024-03-29 22:35:10 +0100

    Update model_inference() function
    Change in the model_inference() function: now only the validation dataset is used
    
    + Small modifications in trainer.py

--- models/tester.py
+++ models/tester.py
@@ -5,7 +5,8 @@
 # @IDE      : PyCharm
 # @Github   : https://github.com/VeritasYin/Project_Orion
 
-from data_loader.data_utils import gen_batch
+#TODO cambiar los imports de TFM
+from data_loader.data_utilsTFM import gen_batch
 from utils.math_utils import evaluation, MAE, descale
 from os.path import join as pjoin
 
@@ -41,7 +42,7 @@ def multi_pred(sess, y_pred, seq, batch_size, n_his, n_pred, step_idx, dynamic_b
         for j in range(n_pred):
             pred = sess.run(y_pred,
                             feed_dict={'data_input:0': test_seq, 'keep_prob:0': 1.0})
-            
+
             ############### Control ###############
             # 50: It is the size of the batch.
             # 50 (batch_size) of all possible "blocks" (which for train would be 9112), are the sequences that we pass to predict.
@@ -49,19 +50,19 @@ def multi_pred(sess, y_pred, seq, batch_size, n_his, n_pred, step_idx, dynamic_b
             # 13: The number of history, what we use to test and not to predict at first.
             # The first 13 (n_his) are used to predict, but then move the window to include the predictions themselves and then the predictions of the predictions.
             ############### Control ###############
-            
+
             # ¿Que hace esto?
             if isinstance(pred, list):
                 pred = np.array(pred[0])
-                
+
             test_seq[:, 0:n_his - 1, :, :] = test_seq[:, 1:n_his, :, :]
             test_seq[:, n_his - 1, :, :] = pred
             step_list.append(pred)
         pred_list.append(step_list)
-        
+
     #  pred_array -> [n_pred, batch_size, n_route, C_0)
     pred_array = np.concatenate(pred_list, axis=1)
-    
+
     return pred_array[step_idx], pred_array.shape[1]
 
 
@@ -92,17 +93,17 @@ def multi_pred_TFM(sess, y_pred, seq, batch_size, n_his, n_pred, step_idx, x_sta
 
             if isinstance(pred, list):
                 pred = np.array(pred[0])
-                
+
             test_seq[:, 0:n_his - 1, :, :] = test_seq[:, 1:n_his, :, :]
             test_seq[:, n_his - 1, :, :] = pred
             step_list.append(pred)
         pred_list.append(step_list)
-        
+
     #  pred_array -> [n_pred, batch_size, n_route, C_0)
     pred_array = np.concatenate(pred_list, axis=1)
 
 
-    
+
         ############################# SAVING EACH YEARS PREDICTIONS #####################################################
     for x in range(step_idx[0]+1):
         print(x, " and ", step_idx[0]+1)
@@ -110,22 +111,22 @@ def multi_pred_TFM(sess, y_pred, seq, batch_size, n_his, n_pred, step_idx, x_sta
         print(seq.shape)
         x_guardar = pd.DataFrame(descale(seq[:, n_his+x, :, 0], x_stats['mean'], x_stats['std'], normalisation))
         x_guardarZ = pd.DataFrame(seq[:, -3+x, :, 0]) # 6-3 --n_his es de 6, el menos 3 es por el espacio!
-        
+
         y_guardar = pd.DataFrame(descale(pred_array[x, :, :, 0], x_stats['mean'], x_stats['std'], normalisation))
         y_guardarZ = pd.DataFrame(pred_array[x, :, :, 0])
         aux_guardar = pd.DataFrame([x_stats['mean'], x_stats['std']])
-        
+
         # 24+x = Año!
         x_guardar.to_csv(f'X_guardado_{24+x}.csv', index=False)
         x_guardarZ.to_csv(f'X_guardado_{24+x}Z.csv', index=False)
         y_guardar.to_csv(f'Y_guardado_{24+x}.csv', index=False)
         y_guardarZ.to_csv(f'Y_guardado_{24+x}Z.csv', index=False)
         aux_guardar.to_csv(f'Aux.csv', index=False)
-    
+
     return pred_array[step_idx], pred_array.shape[1]
 
 
-def model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_va_val, min_val):
+def model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_val):
     '''
     Model inference function.
     :param sess: tf.Session().
@@ -135,29 +136,25 @@ def model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min
     :param n_his: int, the length of historical records for training.
     :param n_pred: int, the length of prediction.
     :param step_idx: int or list, index for prediction slice.
-    :param min_va_val: np.ndarray, metric values on validation set.
-    :param min_val: np.ndarray, metric values on test set.
+    :param min_val: np.ndarray, metric values on validation set.
     '''
-    x_val, x_test, x_stats = inputs.get_data('val'), inputs.get_data('test'), inputs.get_stats()
+    # Validation dataset, dataset statistics and normalization function applied to dataset
+    x_val, x_stats = inputs.get_data('val'), inputs.get_stats()
 
     if n_his + n_pred > x_val.shape[1]:
         raise ValueError(f'ERROR: the value of n_pred "{n_pred}" exceeds the length limit.')
-    
-    # "multi_pred" parece que realiza la predicción de una forma rara para superar los limites de tf.
+
+    # Predictions of the validation dataset
     y_val, len_val = multi_pred(sess, pred, x_val, batch_size, n_his, n_pred, step_idx)
-    # "evaluation" parece que evalua la predicción, por lo que y_val es el valor predecido con x_val, el segmento de validación.
+    # Evaluate the predictions of y_val with the ground truth data stored in x_val.
+    # Note: with step_idx + n_his we get the index of the prediction store in x_val
     evl_val = evaluation(x_val[0:len_val, step_idx + n_his, :, :], y_val, x_stats)
-    
-    
-    # chks: indicator that reflects the relationship of values between evl_val and min_va_val.
-    chks = evl_val < min_va_val
-    # update the metric on test set, if model's performance got improved on the validation.
-    if sum(chks):
-        min_va_val[chks] = evl_val[chks]
-        y_pred, len_pred = multi_pred(sess, pred, x_test, batch_size, n_his, n_pred, step_idx)
-        evl_pred = evaluation(x_test[0:len_pred, step_idx + n_his, :, :], y_pred, x_stats)
-        min_val = evl_pred
-    return min_va_val, min_val
+    # chks: indicator that reflects the relationship of values between evl_val and min_val (array of booleans)
+    chks = evl_val < min_val
+    # Update the metric on validation set if model's performance got improved
+    min_val[chks] = evl_val[chks]
+
+    return min_val
 
 
 def model_test(inputs, batch_size, n_his, n_pred, inf_mode, normalisation, load_path='./output/models/'):
@@ -199,21 +196,21 @@ def model_test(inputs, batch_size, n_his, n_pred, inf_mode, normalisation, load_
          ############################# SAVING AND CONTROL #####################################################
         # The evaluation has been rendered useless.
         # Now to evaluate we compare the results to ARIMA in Jupiter inside "Trials.ipynb"
-        
+
         y_test, len_test = multi_pred_TFM(test_sess, pred, x_test, batch_size, n_his, n_pred, step_idx, x_stats, normalisation)
         evl = evaluation(x_test[0:len_test, step_idx + n_his, :, :], y_test, x_stats)
-        
-        
+
+
         x_tR = descale(x_test[0, -1, :4, 0], x_stats['mean'], x_stats['std'])
         y_tR = descale(y_test[0, 0, :4, 0], x_stats['mean'], x_stats['std'])
-        
+
         x_tR = descale(x_test[0, -1, :, 0], x_stats['mean'], x_stats['std'])
         y_tR = descale(y_test[0, 0, :, 0], x_stats['mean'], x_stats['std'])
         print("Absolute error scaled:        ", MAE(x_test[0, step_idx + n_his, :, 0], y_test[0, 0, :, 0]))
         print("Absolute error de-scaled:   ", MAE(x_tR, y_tR))
-        
 
-        
+
+
 
         for ix in tmp_idx:
             te = evl[ix - 2:ix + 1]
--- models/trainer.py
+++ models/trainer.py
@@ -5,12 +5,14 @@
 # @IDE      : PyCharm
 # @Github   : https://github.com/VeritasYin/Project_Orion
 
-from data_loader.data_utils import gen_batch
+#TODO cambiar los imports de TFM
+from data_loader.data_utilsTFM import gen_batch
 from models.tester import model_inference
 from models.base_model import build_model, model_save
 from os.path import join as pjoin
+from datetime import datetime
 
-# Arreglo para que tensorflow funcione en v1, ya que parece que todo se ha hecho en esa versión. + 
+# Arreglo para que tensorflow funcione en v1, ya que parece que todo se ha hecho en esa versión. +
 import tensorflow
 from tensorflow.compat import v1 as tf
 
@@ -32,7 +34,7 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
     Ks, Kt = args.ks, args.kt
     batch_size, epoch, inf_mode, opt = args.batch_size, args.epoch, args.inf_mode, args.opt
 
-    # Arreglo para quitar el modo "eager" + 
+    # Arreglo para quitar el modo "eager" +
     tf.disable_eager_execution()
 
     # Placeholder for model training, x is  empthy!
@@ -43,9 +45,9 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
     train_loss, pred = build_model(x, n_his, Ks, Kt, blocks, keep_prob)
     tf.summary.scalar('train_loss', train_loss)
     copy_loss = tf.add_n(tf.get_collection('copy_loss'))
-    tf.summary.scalar('copy_loss', copy_loss)  
-            
-            
+    tf.summary.scalar('copy_loss', copy_loss)
+
+
     # Learning rate settings
     global_steps = tf.Variable(0, trainable=False)
     len_train = inputs.get_len('train')
@@ -71,67 +73,94 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
         writer = tf.summary.FileWriter(pjoin(sum_path, 'train'), sess.graph)
         sess.run(tf.global_variables_initializer())
 
+        # step_idx determines which prediction is to be evaluated, min_val store the
         if inf_mode == 'sep':
             # for inference mode 'sep', the type of step index is int.
             step_idx = n_pred - 1
             tmp_idx = [step_idx]
-            min_val = min_va_val = np.array([4e20, 1e20, 1e20]) #np.array([4e1, 1e5, 1e5])
+            min_val = np.array([4e20, 1e20, 1e20]) #np.array([4e1, 1e5, 1e5])
         elif inf_mode == 'merge': # Por defecto
             # for inference mode 'merge', the type of step index is np.ndarray.
             step_idx = tmp_idx = np.arange(3, n_pred + 1, 3) - 1
-            min_val = min_va_val = np.array([4e20, 1e20, 1e20] * len(step_idx)) #np.array([4e1, 1e5, 1e5] * len(step_idx))
+            min_val = np.array([4e20, 1e20, 1e20] * len(step_idx)) #np.array([4e1, 1e5, 1e5] * len(step_idx))
         else:
             raise ValueError(f'ERROR: test mode "{inf_mode}" is not defined.')
 
         # This is where the training happens
         for i in range(epoch):
-            start_time = time.time()
+            t1 = time.time()
             # x_batch are simply train chunks of size ["batch_size", 21, 228, 1].
             # In addition, the loop is going to run len(inputs.get_data('train'))/batch_size, which for the standard case is 182-183 times.
             # n_his, refers to the number of historical records to be used for the prediction, by default 13 of the possible 21 possible records are used.
             for j, x_batch in enumerate(
                     gen_batch(inputs.get_data('train'), batch_size, dynamic_batch=True, shuffle=True)):
                 # Train_op = Train optimizers
-             
-                
+
                 # Enter data to make predictions.
                 # Calculates the train_loss with the X (originally 8) rows that it does not use.
                 # As the "train_op" is entered, this is where the backpropagation is done, because for that you need an optimizer!!!
-                # Reminder, that the last of the x's is used as the y, i.e. if from [0:n_his + 1] go thirteen, 
+                # Reminder, that the last of the x's is used as the y, i.e. if from [0:n_his + 1] go thirteen,
                     # 12 will be used to make a prediction and this will be compared to the remaining thirteen to calculate the loss.
+                #TODO comprobar si se puede mejorar el proceso de entrenamiento (se entrena con npred = 1 y se desperdicia el resto)
                 summary, w = sess.run([merged, train_op], feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1})
                 writer.add_summary(summary, i * epoch_step + j)
 
-                
-                
-                # Check every 50 iterations!
-                if j % 50 == 0: # if j % 50 == 0:
+                # Check every 10 iterations!
+                if j % 10 == 0: # if j % 10 == 0:
                     loss_value = \
                         sess.run([train_loss, copy_loss],
                                  feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1.0})
                     print(f'Epoch {i:2d}, Step {j:3d}: [{loss_value[0]:.3f}, {loss_value[1]:.3f}]')
 
-            print(f'Epoch {i:2d} Training Time {time.time() - start_time:.3f}s')
-                
-            start_time = time.time()
-            min_va_val, min_val = \
-                model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_va_val, min_val)
+            t2 = time.time()
 
+            Info = "Epoch: {epoch}, Training Time: {time}"
+            print(Info.format(epoch = i, time = time.strftime("%H:%M:%S", time.gmtime(t2 - t1))))
+
+            print(type(inputs.get_data('val')))
+
+            # if(inputs.get_data('val') != None):
+
+            #     t1 = time.time()
+
+            #     min_val = model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_val)
+            #     for ix in tmp_idx:
+            #         va = min_val[ix - 2:ix + 1]
+            #         print(f'Time Step {ix + 1}: '
+            #             f'MAPE {va[0]:7.3%}; '
+            #             f'MAE  {va[1]:4.3f}; '
+            #             f'RMSE {va[2]:6.3f}.')
+
+            #     t2 = time.time()
+
+            #     Info = "Epoch: {epoch}, Inference Time: {time}"
+            #     print(Info.format(epoch = i, time = time.strftime("%H:%M:%S", time.gmtime(t2 - t1))))
+
+            #     summary = tf.Summary(value=[tf.Summary.Value(tag='Validation Loss', simple_value=min_val[2])])
+
+            t1 = time.time()
+
+            min_val = model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_val)
             for ix in tmp_idx:
-                va, te = min_va_val[ix - 2:ix + 1], min_val[ix - 2:ix + 1]
+                va = min_val[ix - 2:ix + 1]
                 print(f'Time Step {ix + 1}: '
-                      f'MAPE {va[0]:7.3%}, {te[0]:7.3%}; '
-                      f'MAE  {va[1]:4.3f}, {te[1]:4.3f}; '
-                      f'RMSE {va[2]:6.3f}, {te[2]:6.3f}.')
-            print(f'Epoch {i:2d} Inference Time {time.time() - start_time:.3f}s')
-            
-            summary = tf.Summary(value=[tf.Summary.Value(tag='Validation Loss', simple_value=min_va_val[2])])
+                    f'MAPE {va[0]:7.3%}; '
+                    f'MAE  {va[1]:4.3f}; '
+                    f'RMSE {va[2]:6.3f}.')
+
+            t2 = time.time()
+
+            Info = "Epoch: {epoch}, Inference Time: {time}"
+            print(Info.format(epoch = i, time = time.strftime("%H:%M:%S", time.gmtime(t2 - t1))))
+
+            summary = tf.Summary(value=[tf.Summary.Value(tag='Validation Loss', simple_value=min_val[2])])
 
             # add the summary to the writer
             writer.add_summary(summary, global_step=i)
-            
+
             if (i + 1) % args.save == 0:
                 model_save(sess, global_steps, 'STGCN')
+
         writer.flush()
         writer.close()
     print('Training model finished!')
